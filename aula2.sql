45c95efa7568ef5a2c9cfa422e9adf2ceb405c74
labsfiap <labsfiap@PA304MICRO20.fiap.com.br>
2025-02-10 22:40:50 -0300

    Subindo os inserts das tabelas

---
+++ aula2.sql
@@ -0,0 +1,74 @@
+INSERT INTO pais (id_pais, nomes_pais)
+VALUES (1, 'Brasil');
+
+INSERT INTO pais (id_pais, nomes_pais)
+VALUES (2, 'Argentina');
+
+INSERT INTO pais (id_pais, nomes_pais)
+VALUES (3, 'Venezuela');
+
+INSERT INTO pais (id_pais, nomes_pais)
+VALUES (4, 'Turquia');
+
+INSERT INTO pais (id_pais, nomes_pais)
+VALUES (5, 'Dinamarca');
+
+
+
+INSERT INTO estado (id_estado, nome_estado)
+VALUES (1, 'São Paulo');
+
+INSERT INTO estado (id_estado, nome_estado)
+VALUES (2, 'Ámapá');
+
+INSERT INTO estado (id_estado, nome_estado)
+VALUES (3, 'Acre');
+
+INSERT INTO estado (id_estado, nome_estado)
+VALUES (4, 'Distrito Federal');
+
+INSERT INTO estado (id_estado, nome_estado)
+VALUES (5, 'Tocantis');
+
+
+
+INSERT INTO bairro (id_bairro, nome_bairro)
+VALUES (1, 'Itaquera');
+
+INSERT INTO bairro (id_bairro, nome_bairro)
+VALUES (2, 'Tatuape');
+
+INSERT INTO bairro (id_bairro, nome_bairro)
+VALUES (3, 'Jardim Nossa senhora'); 
+
+INSERT INTO bairro (id_bairro, nome_bairro)
+VALUES (4, 'Diadema');
+
+INSERT INTO bairro (id_bairro, nome_bairro)
+VALUES (5, 'Carrão');
+
+
+INSERT INTO cidade (id_cidade, nome_cidade)
+VALUES (1, 'Buenos Aires');
+
+INSERT INTO cidade (id_cidade, nome_cidade)
+VALUES (2, 'Santiago');
+
+INSERT INTO cidade (id_cidade, nome_cidade)
+VALUES (3, 'Rio Branco');
+
+INSERT INTO cidade (id_cidade, nome_cidade)
+VALUES (4, 'Orlando');
+
+INSERT INTO cidade (id_cidade, nome_cidade)
+VALUES (5, 'Los angeles');
+
+
+INSERT INTO ENDERECO_CLIENTE (id_endereco, cep, logradouro, numero,referencia) VALUES (1, 08280590,'Rua Heitor', 188, 'Portão Cinza');
+INSERT INTO ENDERECO_CLIENTE (id_endereco, cep, logradouro, numero,referencia) VALUES (2, 08280230,'Rua Pedro', 200, 'Portão Branco');
+INSERT INTO ENDERECO_CLIENTE (id_endereco, cep, logradouro, numero,referencia) VALUES (3, 0388647,'Rua Joao', 220, 'Portão Amarelo');
+INSERT INTO ENDERECO_CLIENTE (id_endereco, cep, logradouro, numero,referencia) VALUES (4, 01001220,'Rua Gabriel', 259, 'Portão Azul');
+INSERT INTO ENDERECO_CLIENTE (id_endereco, cep, logradouro, numero,referencia) VALUES (5, 03886470,'Rua Marcos Gago', 100, 'Marquinhos com a lingua');
+
+
+
